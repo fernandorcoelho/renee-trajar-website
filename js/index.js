1eb40a7fa6028f7a2033a937eebb5273fa896c9f
Fernando Rodrigues Coelho <fernando.ufv14@gmail.com>
2021-10-27 13:13:54 -0300

    Finished Mobile Menu

--- js/index.js
+++ js/index.js
@@ -58,3 +58,13 @@ $(window).scroll(function() {
         }
     });
 });
+
+$(document).ready(function() {
+    var $toggleButton = $('.toggle-button'),
+        $menuWrap = $('.menu-wrap');
+
+    $toggleButton.on('click', function() {
+        $(this).toggleClass('button-open');
+        $menuWrap.toggleClass('menu-show');
+    });
+});
